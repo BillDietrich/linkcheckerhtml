6ca709fa32b05dd3b8545e1e125fd297334bf17e
Bill Dietrich <bill_dietrich@protonmail.com>
2018-11-24 12:51:47 +0100

    first commit

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
-THIS PROJECT IS NOT USABLE YET !!!
+THIS PROJECT HASN'T BEEN TESTED MUCH YET !!!
 
 
 ##Functionality
 
-Load an HTML file, then use Alt+L, and it will generate a report on the links in the document, including broken links. It attempts to check broken links by trying to resolve HTTP & HTTPS links, and relative links (../folder/file.html) by checking if the file exist on the local file system. The result of these checks are logged in an output window on the right of the editor.
+Check for broken Anchor links in HTML documents. It checks broken links by trying to resolve HTTP and HTTPS links, and relative links (../folder/file.html) by checking if the file exist on the local file system.
 
 Note that checking for broken links is more of an art than a science. Some sites don't actually return 404, but send you to a landing page. For example, Azure.com works this way. You can go to https://Azure.com/foo/bar and it will happily redirect you to https://Azure.com, with no 404 status returned. So take a status of "OK" with a grain of salt - you may not be arriving at the page you intend.
 
@@ -14,21 +14,27 @@ Open Visual Studio Code and press `F1`; a field will appear at the top of the wi
 
 ##Check for broken links
 
-To check for broken links, use Alt+L. This will open a new column to the right of the VSCode window and display the status of the links as they are checked.
+To check for broken links, open an HTML file and then press Alt+L.  See the standard error/warn/info diagnostic icons in lower-left of UI.
 
 ##Changes
 
 ###0.2
 
 * Copied from "Microsoft / linkcheckermd"
-* Extension works, but output on debug channel only
+* Extension works, but positions on lines are wrong, probably has memory leaks, not much testing.
 
 ##TODO
 
-* Output into a normal window.
-* Click on a broken-link notification and go to that line in source file.
+* Fix line-character-nums in diagnostics.
+* Memory leaks ?
+* Deactivate().
+* Test outside debug environment.
+* Hard-coded paths.
+* Warn about redirects.
+* File:// scheme versus native pathname.
+* Case-insensitive in Anchor tag ?
+* Local copy of node_modules tree.
 * Register extension in Marketplace.
-* Refactor broken link checking to display the actual URL that you arrived at for "OK" results that were redirects to a different URL.
 
 ##Development
 
@@ -38,7 +44,6 @@ I used these things:
 * VSCode 1.29.1 (which says Node.js 8.9.3)
 * node 8.10.0
 * npm 3.5.2
-* sudo npm -g install --save rsvp
 * sudo npm -g install --save broken-link
 * Yeoman
 
--- extension.ts
+++ extension.ts
@@ -16,7 +16,8 @@ import {
     TextLine,
     ViewColumn} from 'vscode';
 // replacement for Promise which was removed from Node.js circa 2016
-import rsvp = require('rsvp');
+//var promise = require('pinkie-promise');
+//import rsvp = require('rsvp');
 import fs = require('fs');
 // For checking relative URIs against the local file system
 import path = require('path');
@@ -31,35 +32,70 @@ interface Link {
     lineText: TextLine
 }
 
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(disposables: Disposable[]) {     
-    //console.log("linkcheckerhtml: active");
+    //let outputChannel = window.createOutputChannel("linkcheckerhtml");
+    // Show the output channel
+    //outputChannel.show(false);	// preserveFocus == false
+    //outputChannel.appendLine(`linkcheckerhtml.activate: active`);
+	//outputChannel.appendLine(`linkcheckerhtml.activate: uri = ${window.activeTextEditor.document.uri.toString()}`);
+    //outputChannel.appendLine(`linkcheckerhtml.activate: visibleTextEditors.length = ${window.visibleTextEditors.length}`);
+	
+
+/*
+    var diagnosticsCollection = languages.createDiagnosticCollection("linkcheckerhtml");
+	var diagnosticsArray = new Array<Diagnostic>();
+	var diag = new Diagnostic(new Range(new Position(1,10),new Position(2,20)), "message", DiagnosticSeverity.Error);
+	diagnosticsArray.push(diag);
+	diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
+*/
+
     commands.registerCommand('extension.generateLinkReport', generateLinkReport);
-    //console.log("linkcheckerhtml: finished activation");
+
+    //outputChannel.appendLine(`linkcheckerhtml.activate: finished`);
 }
 
 
 // Generate a report of broken links and the line they occur on
 function generateLinkReport() {
     // Get the current document
-    let document = window.activeTextEditor.document;
+    var document = window.activeTextEditor.document;
+
     // Create an output channel for displaying broken links
-    let outputChannel = window.createOutputChannel("linkcheckerhtml");
+    //var outputChannel = window.createOutputChannel("linkcheckerhtml2");
     // Show the output channel
-    outputChannel.show(false);	// preserveFocus == false
-    //outputChannel.appendLine(`linkcheckerhtml: generateLinkReport called`);
+    //outputChannel.show(true);	// preserveFocus
+    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: called`);
+    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: uri = ${document.uri.toString()}`);
+    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: visibleTextEditors.length = ${window.visibleTextEditors.length}`);
+    //document = window.visibleTextEditors[1].document;
 
-	//let webviewPanel = window.createWebviewPanel("", "linkcheckerhtml", ViewColumn.Beside);
-
-    // Get all HTML Anchor lnks in the document
+    // Get all HTML Anchor links in the document
     getLinks(document).then((links) => {
-	    //outputChannel.appendLine(`linkcheckerhtml: got ${links.length} links`);
+		// Promise resolved now, so we're in a different context than before
+	    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: got ${links.length} links`);
+
+	    var diagnosticsCollection = languages.createDiagnosticCollection("linkcheckerhtml");
+		var diagnosticsArray = new Array<Diagnostic>();
+		var diag = null;
+
+/*
+		diag = new Diagnostic(new Range(new Position(1,10),new Position(2,20)), "messageHHHH", DiagnosticSeverity.Error);
+		diagnosticsArray.push(diag);
+		diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
+
+	    diagnosticsArray = languages.getDiagnostics(window.activeTextEditor.document.uri);
+		diag = new Diagnostic(new Range(new Position(1,10),new Position(2,20)), "messageIIII", DiagnosticSeverity.Error);
+		diagnosticsArray.push(diag);
+		diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
+*/
+
         // Loop over the links
         links.forEach(link => {
-		    //outputChannel.appendLine(`linkcheckerhtml: link on line ${link.lineText.lineNumber + 1} is "${link.address}"`);
-            // Get the line number, because internally it's 0 based, but not in display
-            let lineNumber = link.lineText.lineNumber + 1;
+		    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: link on line ${link.lineText.lineNumber + 1} is "${link.address}"`);
+            var lineNumber = link.lineText.lineNumber;
             
             // Is it an HTTP* link or a relative link?
             if(isHttpLink(link.address)) {
@@ -67,45 +103,60 @@ function generateLinkReport() {
                 brokenLink(link.address, {allowRedirects: true}).then((answer) => {
                     // Log to the outputChannel
                     if(answer) {
-                        outputChannel.appendLine(`Error: ${link.address} on line ${lineNumber} is unreachable.`);
+                        //outputChannel.appendLine(`Error: ${link.address} on line ${lineNumber} is unreachable.`);
+					    diagnosticsArray = languages.getDiagnostics(window.activeTextEditor.document.uri);
+						//diag = new Diagnostic(new Range(new Position(15,10),new Position(5,20)), "messageZZZZZZZZ", DiagnosticSeverity.Error);
+						diag = new Diagnostic(new Range(new Position(lineNumber,2),new Position(lineNumber,20)), `${link.address} is unreachable.`, DiagnosticSeverity.Error);
+						diagnosticsArray.push(diag);
+						diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
                     } else {
-                        outputChannel.appendLine(`Info: ${link.address} on line ${lineNumber} is accessible.`);
+                        //outputChannel.appendLine(`Info: ${link.address} on line ${lineNumber} is accessible.`);
                     }
                 });
             } else {
                 if(isNonHTTPLink(link.address)) {
                     // We don't do anything with other URL types
-                    outputChannel.appendLine(`Info: ${link.address} on line ${lineNumber} is non-HTTP* link; not checked.`);
+                    //outputChannel.appendLine(`Info: ${link.address} on line ${lineNumber} is non-HTTP* link; not checked.`);
+					diagnosticsArray = languages.getDiagnostics(window.activeTextEditor.document.uri);
+					diag = new Diagnostic(new Range(new Position(lineNumber,2),new Position(lineNumber,20)), `${link.address} is non-HTTP* link; not checked.`, DiagnosticSeverity.Information);
+					diagnosticsArray.push(diag);
+					diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
                 } else {
                     // Must be a relative path, but might not be, so try it...
                     try {
                         // Find the directory from the path to the current document
-                        let currentWorkingDirectory = path.dirname(document.fileName);
+                        var currentWorkingDirectory = path.dirname(document.fileName);
                         // Use that to resolve the full path from the relative link address
                         // The `.split('#')[0]` at the end is so that relative links that also reference an anchor in the document will work with file checking.
-                        let fullPath = path.resolve(currentWorkingDirectory, link.address).split('#')[0];
+                        var fullPath = path.resolve(currentWorkingDirectory, link.address).split('#')[0];
                         // Check if the file exists and log appropriately
                         if(fs.existsSync(fullPath)) {
-                            outputChannel.appendLine(`Info: local file ${link.address} on line ${lineNumber} exists.`);
+                            //outputChannel.appendLine(`Info: local file ${link.address} on line ${lineNumber} exists.`);
                         } else {
-                            outputChannel.appendLine(`Error: local file ${link.address} on line ${lineNumber} does not exist.`);
+                            //outputChannel.appendLine(`Error: local file ${link.address} on line ${lineNumber} does not exist.`);
+						    diagnosticsArray = languages.getDiagnostics(window.activeTextEditor.document.uri);
+							diag = new Diagnostic(new Range(new Position(lineNumber,2),new Position(lineNumber,20)), `Local file ${link.address} does not exist.`, DiagnosticSeverity.Error);
+							diagnosticsArray.push(diag);
+							diagnosticsCollection.set(window.activeTextEditor.document.uri,diagnosticsArray);
                         }
                     } catch (error) {
                         // If there's an error, log the link
-                        outputChannel.appendLine(`Error: ${link.address} on line ${lineNumber} is not an HTTP/s or relative link.`);
+                        //outputChannel.appendLine(`Error: ${link.address} on line ${lineNumber} is not an HTTP/s or relative link.`);
                     }
                 }
             }
         });
+
     });
-    //outputChannel.appendLine(`linkcheckerhtml: generateLinkReport returning`);
+
+    //outputChannel.appendLine(`linkcheckerhtml.generateLinkReport: returning`);
 }
  
 // Parse the HTML Anchor links out of the document
-function getLinks(document: TextDocument): rsvp.Promise<Link[]> {
-    console.log("linkcheckerhtml: getLinks called");
+function getLinks(document: TextDocument): Promise<Link[]> {
+    //console.log("linkcheckerhtml: getLinks called");
     // Return a promise, since this might take a while for large documents
-    return new rsvp.Promise<Link[]>((resolve, reject) => {
+    return new Promise<Link[]>((resolve, reject) => {
         // Create arrays to hold links as we parse them out
         let linksToReturn = new Array<Link>();
         // Get lines in the document
@@ -117,13 +168,16 @@ function getLinks(document: TextDocument): rsvp.Promise<Link[]> {
             let lineText = document.lineAt(lineNumber);
             // Are there links?
 			// HTML link looks like: <a href="urlhere">
-            let links = lineText.text.match(/<[aA]\s+[hH][rR][eE][fF]="[^"]*"/g);
+            let links = lineText.text.match(/<a\s+href="[^"]*"/g);
             if(links) {
                 // Iterate over the links found on this line
                 for(let i = 0; i< links.length; i++) {
                     // Get the URL from each individual link
-                    var link = links[i].match(/<[aA]\s+[hH][rR][eE][fF]="([^"]*)"/);
+                    var link = links[i].match(/<a\s+href="([^"]*)"/);
                     let address = link[1];
+					//var regex = /<a\s+href="([^"]*)"/i;
+					//var match = regex.exec(links[i]);
+					//var address = match[0];
                     //Push it to the array
                     linksToReturn.push({
                         text: link[0],
@@ -175,3 +229,18 @@ function isNonHTTPLink(linkToCheck: string): boolean {
     return bRetVal;
 }
 
+
+// Generate a diagnostic object
+function OldcreateDiagnostic(severity: DiagnosticSeverity, httpLink, lineText: TextLine, message): Diagnostic {
+    // Get the location of the text in the document
+    // based on position within the line of text it occurs in
+    let startPos = lineText.text.indexOf(httpLink);
+    let endPos = startPos + httpLink.length -1;
+    let start = new Position(lineText.lineNumber,startPos);
+    let end = new Position(lineText.lineNumber, endPos);
+    let range = new Range(start, end);
+    // Create the diagnostic object
+    let diag = new Diagnostic(range, message, severity);
+    // Return the diagnostic
+    return diag;
+}
